efdf7956fa579f80013a8d1f6a41399f2db175ab
Szabboy17 <129592254+Szabboy17@users.noreply.github.com>
2023-09-24 15:21:08 +0200

    Add files via upload

---
+++ enter.js
@@ -0,0 +1,62 @@
+
+    
+
+function Click(){
+    window.location = "paulaner.html"
+};
+
+function Click1(){
+    window.location = "warsteiner.html"
+};
+
+function Click2(){
+    window.location = "cornelissen.html"
+};
+
+function Click3(){
+    window.location = "konig.html"
+};
+
+function Click4(){
+    window.location = "limburse.html"
+};
+function Click5(){
+    window.location = "ludwig.html"
+};
+
+
+
+
+
+function myFunction(elmnt, clr) {
+  elmnt.style.color = clr;
+};
+
+function ClickHun(){
+    if(Click)
+    window.location= "sorhaz.html"
+};
+
+function ClickEn(){
+    
+    document.getElementById("phar").innerHTML = "Beer Bar"
+    document.getElementById("fooldal").innerHTML = "Home"
+    document.getElementById("rolunk").innerHTML = "About us"
+    document.getElementById("termekek").innerHTML = "Products"
+    document.getElementById("kapcsolat").innerHTML = "Contact"
+    document.getElementById("demo").innerHTML = "Its special quality, delicately spiced, full flavor made Münchner Premium Lager one of the most popular beers in Bavaria. Thanks to its balanced aroma, it can be consumed excellently with any food."
+    document.getElementById("pauen").innerHTML = "A traditional concoction made in the Bavarian stronghold. Generations have handed down the ancient recipe for this nobly full-flavored beer. Its special quality, delicately spiced, full flavor made Münchner Premium Lager one of the most popular beers in Bavaria. Thanks to its balanced aroma, it can be consumed excellently with any food."
+    document.getElementById("empl").innerHTML = "Our employees"
+    document.getElementById("warste").innerHTML = "Unripened Kellerbier, aromatic pleasant bitterness, finely balanced caramel and honeyc accompanied by tickets. A special aroma hop, so called Brewers Gold, gives it its unique character. This beer specialty also shows that modern taste does not have to compromise with traditional brewing. Warsteiner Brewers Gold is the perfect choice for every occasion! Alcohol 5.2%."
+    document.getElementById("corne").innerHTML = "The perfect marriage of authentic Belgian pils and blueberry. What makes it so special? From its sophisticated aftertaste, which is the result of the special cooking process."
+    document.getElementById("konigl").innerHTML = "One of Germany's most famous brown beers. The roasted barley malt gives the beer its characteristic color and lends it a caramel-like taste."
+    document.getElementById("limburse").innerHTML = "Limburgse Witte Peach & Mango 0.33L disposable glass Belgian craft wheat beer infused with Mango and Peach fruitiness."
+    document.getElementById("hefe").innerHTML = "König Ludwig Hefe Weissbier unfiltered wheat beer in a 0.5l disposable bottle. The royal wheat beer! Golden yellow, unripened, slightly malty wheat beer with a fruity character."
+    document.getElementById("kissa").innerHTML = "András Kiss waiter"
+    document.getElementById("kissj").innerHTML = "Julianna Kiss waitress"
+    document.getElementById("kovj").innerHTML = "János Kovács cup holder"
+    document.getElementById("juri").innerHTML = "Ildikó Jurásek bartender"
+    document.getElementById("nagys").innerHTML = "Sándor Nagy waiter"
+    document.getElementById("totop").innerHTML = "Jump to the top!"
+
+}
---
+++ kor.js
@@ -0,0 +1,21 @@
+
+function Click(){
+if(Click)
+
+    
+    window.location="sorhaz.html";
+};
+
+function Click3(){
+    if(Click)
+    
+        
+        window.location="fekete.html";
+    };
+
+
+function Click2(){
+    if(Click){
+        window.location="index.html"
+    }
+}
